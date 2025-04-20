8592f66fbe09c10f4f99dd8495b04fdcde845757
Lucas Borges <lucasbggv2001@gmail.com>
2025-04-20 10:12:05 -0300

    Actualización de los materiales del laboratorio

--- AFND_e.py
+++ AFND_e.py
@@ -1,4 +1,6 @@
 from collections import defaultdict, deque
+from itertools import permutations
+
 
 class AFND_e:
     def __init__(self):
@@ -34,46 +36,89 @@ class AFND_e:
 
     @staticmethod
     def son_isomorfos(afnd1, afnd2):
-        if afnd1 is not None and afnd2 is not None:
-            if len(afnd1.estados) != len(afnd2.estados):
-                return False
-            if len(afnd1.finales) != len(afnd2.finales):
-                return False
-            if afnd1.inicial is None or afnd2.inicial is None:
-                return False
+        if len(afnd1.estados) != len(afnd2.estados):
+            return False
+        if len(afnd1.finales) != len(afnd2.finales):
+            return False
+        if afnd1.inicial is None or afnd2.inicial is None:
+            return False
 
-            mapeo = {}
-            visitados = set()
-            cola = deque([(afnd1.inicial, afnd2.inicial)])
-            mapeo[afnd1.inicial] = afnd2.inicial
+        mapeo = {}
+        visitados = set()
+        cola = deque([(afnd1.inicial, afnd2.inicial)])
+        mapeo[afnd1.inicial] = afnd2.inicial
 
-            while cola:
-                e1, e2 = cola.popleft()
-                if (e1 in afnd1.finales) != (e2 in afnd2.finales):
+        while cola:
+            e1, e2 = cola.popleft()
+            if (e1 in afnd1.finales) != (e2 in afnd2.finales):
+                return False
+            visitados.add((e1, e2))
+
+            trans1 = afnd1.transiciones.get(e1, {})
+            trans2 = afnd2.transiciones.get(e2, {})
+
+            if set(trans1.keys()) != set(trans2.keys()):
+                return AFND_e.son_isomorfos_extended(afnd1, afnd2)
+
+            for simbolo in trans1:
+                destinos1 = sorted(trans1[simbolo])
+                destinos2 = sorted(trans2[simbolo])
+
+                if len(destinos1) != len(destinos2):
                     return False
-                visitados.add((e1, e2))
 
-                trans1 = afnd1.transiciones.get(e1, {})
-                trans2 = afnd2.transiciones.get(e2, {})
+                for d1, d2 in zip(destinos1, destinos2):
+                    if d1 in mapeo:
+                        if mapeo[d1] != d2:
+                            return False
+                    else:
+                        mapeo[d1] = d2
+                        if (d1, d2) not in visitados:
+                            cola.append((d1, d2))
+
+        return True
+
+    @staticmethod
+    def son_isomorfos_extended(afnd1, afnd2):
+        if len(afnd1.estados) != len(afnd2.estados):
+            return False
+        if len(afnd1.finales) != len(afnd2.finales):
+            return False
+
+        estados1 = list(afnd1.estados)
+        estados2 = list(afnd2.estados)
+
+        for perm in permutations(estados2):
+            mapping = dict(zip(estados1, perm))
+
+            if mapping[afnd1.inicial] != afnd2.inicial:
+                continue
+
+            finales_mapeados = {mapping[f] for f in afnd1.finales}
+            if finales_mapeados != set(afnd2.finales):
+                continue
+
+            match = True
+            for estado1 in afnd1.transiciones:
+                trans1 = afnd1.transiciones[estado1]
+                estado2 = mapping[estado1]
+                trans2 = afnd2.transiciones.get(estado2, {})
 
                 if set(trans1.keys()) != set(trans2.keys()):
-                    return False
+                    match = False
+                    break
 
                 for simbolo in trans1:
-                    destinos1 = sorted(trans1[simbolo])
-                    destinos2 = sorted(trans2[simbolo])
+                    destinos1 = {mapping[d] for d in trans1[simbolo]}
+                    destinos2 = set(trans2.get(simbolo, []))
+                    if destinos1 != destinos2:
+                        match = False
+                        break
 
-                    if len(destinos1) != len(destinos2):
-                        return False
+                if not match:
+                    break
 
-                    for d1, d2 in zip(destinos1, destinos2):
-                        if d1 in mapeo:
-                            if mapeo[d1] != d2:
-                                return False
-                        else:
-                            mapeo[d1] = d2
-                            if (d1, d2) not in visitados:
-                                cola.append((d1, d2))
+            if match:
+                return True
 
-            return True
         return False
